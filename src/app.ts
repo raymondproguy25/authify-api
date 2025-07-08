32f69575e6bd60a2b0b32b4fc70c9777d862d0d5
raymondproguy25 <raymondnicholas833@gmail.com>
2025-07-08 21:49:54 +0100

    feat: setup express with tsconfig and npm script

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.json({ message: 'Welcome to Authify API.' });
+});
+
+const PORT = process.env.PORT || 5000;
+ app.listen(PORT, () => {
+ console.log(`Server runinig on http://localhost/${PORT}`)
+});
