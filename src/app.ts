382c25f58ad1b3f11735b8566c68b4c391dd904c
raymondproguy25 <raymondnicholas833@gmail.com>
2025-07-09 22:55:39 +0100

    fix: call conectioDB() to resolve promise and start the server

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import connectDB from "../src/config/database.js";
+import connectDB from './config/database.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -11,7 +11,7 @@ app.use(express.json());
 app.get('/', (_req, res) => {
   res.json({ message: 'Welcome to Authify API.' });
 });
-connectDB.then(() =>{
+connectDB().then(() =>{
 const PORT = process.env.PORT || 5000;
  app.listen(PORT, () => {
  console.log(`Server runinig on http://localhost:${PORT}`)
